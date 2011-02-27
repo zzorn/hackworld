4e3fc12ca93a7836b258c4da13e275e9f644548d
zzorn <zzorn@iki.fi>
2011-02-27 17:39:20 +0200

    Adding some moved facet classes that went missing from last commit.

---
+++ src/main/scala/hackworld/spacegame/entity/FacetManager.scala
@@ -0,0 +1,24 @@
+package hackworld.spacegame.entity
+
+
+/**
+ * 
+ */
+object FacetManager {
+
+  private var facets: Map[Symbol, Manifest[_ <: Facet]] = Map()
+
+  def registerFacetType[T <: Facet](implicit m: Manifest[T]) {
+    val sym = Symbol(m.erasure.getSimpleName)
+    println("registering facet " + sym.name)
+    facets += (sym -> m)
+  }
+
+  def createFacet(name: Symbol): Option[(Facet, Manifest[Facet])] = {
+    facets.get(name) match {
+      case None => None
+      case Some(m) => Some((m.erasure.newInstance().asInstanceOf[Facet], m.asInstanceOf[Manifest[Facet]]))
+    }
+  }
+
+}
